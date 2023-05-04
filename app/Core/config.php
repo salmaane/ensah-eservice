e8d2cfdf7ed18ea18b26d1d2071a7d457c0bd4db
Salmane <elmourtajisalmane@gmail.com>
2023-05-04 17:51:50 +0100

    set configurations

--- app/Core/config.php
+++ app/Core/config.php
@@ -5,16 +5,16 @@ define('SITE_NAME','php-simple-mvc');
 
 // App Root
 define('APP_ROOT',dirname(dirname(dirname(__FILE__))));
-define('ROOT_DIRNAME','sm-php-mvc');
-define('URL_ROOT','');
+define('ROOT_DIRNAME','ensah-eservice');
+define('URL_ROOT','http://127.0.0.1/ensah-eservice/');
 define('URL_SUBFOLDER', '');
 
 // DB Params
-define('DB_DRIVER','');
-define('DB_HOST','');
-define('DB_NAME','');
-define('DB_USER','');
-define('DB_PASS','');
+define('DB_DRIVER','mysql');
+define('DB_HOST','localhost');
+define('DB_NAME','ensah');
+define('DB_USER','root');
+define('DB_PASS','salmane');
 
 // path config
 define('PATHS', [
